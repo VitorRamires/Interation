2cd72e05ac9303e66697a67eabd0b69ed33aee80
Vitor Ramires <vmr.rs@hotmail.com>
2026-04-02 12:41:43 -0300

    update: new error message

--- src/components/errorPage.jsx
+++ src/components/errorPage.jsx
@@ -1,6 +1,9 @@
+import alertIcon from "../assets/icons/alert.svg"
+
 export function ErrorPage() {
   return (
     <div className="error">
+      <img src={alertIcon} />
       <p>An error Occurr! Try later</p>
     </div>
   );
